4674bad2ff4d3947b53791177721c90559fe620e
hgnagendra <30690813+hgnagendra@users.noreply.github.com>
2017-08-09 20:38:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,9 +5,16 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/articleone', function (req, res) {
+  res.send("article oneis requested");
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));

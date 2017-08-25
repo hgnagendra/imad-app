5c56e37372752ceaff9c004d403bada699ab47b1
hgnagendra <30690813+hgnagendra@users.noreply.github.com>
2017-08-25 16:18:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -157,12 +157,12 @@ app.get('/:articlename', function(req, res) {
 
 app.get('/article-three', function(req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
+}); */
 
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-}); */
+}); 
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
